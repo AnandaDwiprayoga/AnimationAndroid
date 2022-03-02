df25ad4ecc8096db2ac1189f4644a6980e8ca5f7
Ananda Dwi Prayoga <prayogaananda04@gmail.com>
2022-03-02 21:56:10 +0700

    complete motion layout

---
+++ app/src/main/java/com/pasukanlangit/id/motionlayout/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.pasukanlangit.id.motionlayout
+
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.WindowInsets
+import android.view.WindowManager
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        setFullScreen()
+    }
+
+    //hide action bar and status bar
+    private fun setFullScreen() {
+        @Suppress("DEPRECATION")
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.R){
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        }else{
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
+        }
+        supportActionBar?.hide()
+    }
+}
